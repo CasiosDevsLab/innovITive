a2b2c6615e4f7e839c214a5386200d028e3ab9d7
Lennart Behrendt <lennartbehrendtmail@gmail.com>
2023-09-14 10:56:53 +0200

    CNAME für die custom Domain committed

---
+++ src/app/floating-btn/floating-btn.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-floating-btn',
+  templateUrl: './floating-btn.component.html',
+  styleUrls: ['./floating-btn.component.scss']
+})
+export class FloatingBtnComponent {
+
+}
